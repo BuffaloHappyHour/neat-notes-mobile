9c752715ed88b6f5328534406ca42f8893d26647
Derek <YOUR_GITHUB_EMAIL_HERE>
2026-04-27 09:30:19 -0400

    Bump version to 1.0.6

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
   expo: {
     name: IS_DEV_CLIENT ? "Neat Notes Dev" : "Neat Notes",
     slug: "neat-notes",
-    version: "1.0.5",
+    version: "1.0.6",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: IS_DEV_CLIENT ? "neatnotesdev" : "neatnotes",
@@ -16,7 +16,7 @@ module.exports = {
       bundleIdentifier: IS_DEV_CLIENT
         ? "com.neatnotesapp.neatnotes.dev"
         : "com.neatnotesapp.neatnotes",
-      buildNumber: "47",
+      buildNumber: "48",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
